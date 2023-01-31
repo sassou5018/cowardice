cc3094caedfb43727a7bc4c305bc560d959ae413
Yacinn <sassou.est100@gmail.com>
2023-01-31 23:07:28 +0100

    added form submit func to login page

--- components/login.tsx
+++ components/login.tsx
@@ -26,16 +26,16 @@ export default function Login() {
     
     return (
         
-        <form className=' bg-opacity-50 bg-gray-400 p-6 w-max h-screen flex flex-col justify-center mr-0 sm:mr-96'>
+        <form onSubmit={login} className=' bg-opacity-50 bg-gray-400 p-6 w-max h-screen flex flex-col justify-center mr-0 sm:mr-96'>
                 <h2 className='text-center text-white text-2xl cursor-default'>Log in</h2>
                 <br />
-                <input type='text' className='rounded-md px-3 py-2 focus:bg-gray-100 text-black focus:outline-none invalid:outline invalid:outline-red-500' placeholder='Enter Your Username'/>
+                <input type='text' name="username" className='rounded-md px-3 py-2 focus:bg-gray-100 text-black focus:outline-none invalid:outline invalid:outline-red-500' placeholder='Enter Your Username'/>
                 <br />
-                <input type='password' className='rounded-md px-3 py-2 focus:bg-gray-100 text-black focus:outline-none invalid:outline invalid:outline-red-500' placeholder='********' />
+                <input type='password' name='password' className='rounded-md px-3 py-2 focus:bg-gray-100 text-black focus:outline-none invalid:outline invalid:outline-red-500' placeholder='********' />
                 <br />
                 <button className='rounded-md bg-white p-1 w-80 sm:w-96 hover:bg-gray-200'>Log in</button>
                 <br />
-                <Link href='#' className='text-white text-xs hover:underline'>Forgot Password</Link>
+                <Link href='/resetpassword' className='text-white text-xs hover:underline'>Forgot Password</Link>
         </form>
         
     )
