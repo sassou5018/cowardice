102cba805a99406c742d7c3c454b87ccc6afd565
Yacinn <sassou.est100@gmail.com>
2023-02-10 23:14:17 +0100

    removed async

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,7 +1,7 @@
 import UpdateUserName from "@/components/updateUsername";
 
 
-export default async function Page(){
+export default function Page(){
     return(
         <main className="bg-base-100 flex flex-col justify-center items-center p-3">
                 <h1>Settings</h1>
