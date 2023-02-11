8b249e217f64bf506106d4d33654320a4f8a7d7b
Yacinn <sassou.est100@gmail.com>
2023-02-11 01:54:34 +0100

    changed background color daisy ui fucked up my fucking desing

--- components/fullArticle/fullArticle.tsx
+++ components/fullArticle/fullArticle.tsx
@@ -15,18 +15,18 @@ export default function FullArticle({title, author, date, content}: FullArticleP
         __html: content
     }
     return (
-        <div id='article' className='prose mockup-window border border-base-300 bg-neutral text-primary-content p-5'>
+        <div id='article' className='prose mockup-window border border-base-300 bg-white text-black p-5'>
             <div id='header' className='text-white'>
-                <h1 className='font-serif text-3xl'>{title}</h1>
-                <h3 className='text-sm font-serif'>by <Link className="link" target="_blank" href={`/author/${author.username}`}>{author.username}</Link></h3>
-                <p className='text-xs font-semibold'>
-                    Published in <span className='font-light'>{date}</span>
+                <h1 className='font-serif text-3xl text-black'>{title}</h1>
+                <h3 className='text-sm font-serif text-black'>by <Link className="link" target="_blank" href={`/author/${author.username}`}>{author.username}</Link></h3>
+                <p className='text-xs font-semibold text-black'>
+                    Published in <span className='font-light text-black'>{date}</span>
                 </p>
             </div>
-            <div id='content' className=''>
+            <div id='content' className='text-black'>
                 <article
-                    id='content'
-                    className='prose text-white'
+                    id='content text-black'
+                    className='prose'
                     dangerouslySetInnerHTML={html}
                 />
             </div>
